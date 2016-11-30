ea8cc93dbfe5fa80ca6b55304cb52450b2482f51
José Gorgulho <jgorgulho@gmail.com>
2016-11-30 01:09:48 +0000

    Fedora Workstation install should not work as expected

--- fedoraWorkstationInstall.sh
+++ fedoraWorkstationInstall.sh
@@ -19,11 +19,12 @@ sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-skype.repo
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-flash-plugin.repo
 # Multimedia
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo
-sudo dnf install -y @^workstation-product-environment \
-@admin-tools @authoring-and-publishing @books @c-development \ 
-@design-suite @development-tools @editors \
-@headless-management @office @system-tools @text-internet \
-@printing tmux vim git python-pip perl-core \
+sudo dnf install -y Workstation\ Product\ Environment \
+Administration\ Tools Authoring\ and\ Publishing Books C\ Development\ Tools \
+Design\ Suite Development\ Tools Editors \
+Headless\ Management Office System\ Tools Text-based\ Internet \
+Printing 
+sudo dnf install -y tmux vim git python-pip perl-core \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 https://github.com/downloads/djmelik/archey/archey-0.2.6-6.noarch.rpm \
@@ -33,10 +34,14 @@ simple-scan unzip thunderbird gnome-tweak-tool chromium  unrar skype flash-plugi
 google-chrome-stable
 sudo pip install --upgrade pip
 sudo pip install powerline-status
-git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
+if ! [ -d $HOME/.dotfiles ]; then
+    git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
+fi
 ln -fs /home/jgorgulho/.dotfiles/.bashrc /home/jgorgulho/.bashrc
-mkdir /home/jgorgulho/installs
-wget http://prdownloads.sourceforge.net/hplip/hplip-2.16.11.run
-wget https://lastpass.com/lplinux.tar.bz2
+mkdir /home/jgorgulho/toInstall
+wget -O hplip.run "http://downloads.sourceforge.net/project/hplip/hplip/3.16.11/hplip-3.16.11.run?r=http%3A%2F%2Fhplipopensource.com%2Fhplip-web%2    Finstall%2Finstall%2Findex.html&ts=1480467242&use_mirror=netassist"
+wget -O lastpass.tar.bz2 "https://lastpass.com/lplinux.tar.bz2"
+mv hplip.run toInstall
+mv lastpass.tar.bz2 toInstall
 # Enable Disk I/O Scheduler for ssd
 sudo systemctl enable fstrim.timer 
