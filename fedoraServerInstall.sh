ccbff50cc98d87d9e343729458531fde9b14a1de
José Gorgulho <jgorgulho@gmail.com>
2016-11-30 01:00:43 +0000

    Fedora server install should now work as expected

--- fedoraServerInstall.sh
+++ fedoraServerInstall.sh
@@ -1,24 +1,22 @@
 #!/bin/env bash
-sudo dnf install -y @^server-product-environment \
-@admin-tools @authoring-and-publishing @books \
-@c-development @cloud-management @container-management \
-@development-tools @editors @headless-management \
-@printing @server-hardware-support @system-tools \
-@text-internet @virtualization-headless \
-tmux tuned vim git python-pip perl-core \
+sudo dnf groupinstall -y Fedora\ Server\ Edition Administration\ Tools Authoring\ and\ Publishing Books\ and\ Guides C\ Development\ Tools\ and\ Libraries Cloud\ Management\ Tools Container\ Management Development\ Tools Editors Headless\ Management System\ Tools Text-based\ Internet Printing Hardware\ Support virtualization-headless
+sudo dnf install -y tmux tuned vim git python-pip perl-core \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 https://github.com/downloads/djmelik/archey/archey-0.2.6-6.noarch.rpm \
-ruby-devel libxml2-devel rubygem-rails rubygem-sqlite3 rubygem-coffee-rails \ 
-rubygem-sass-rails rubygem-uglifier rubygem-jquery-rails rubygem-turbolinks \
+ruby-devel libxml2-devel rubygem-rails rubygem-sqlite3 rubygem-coffee-rails \
+rubygem-uglifier rubygem-jquery-rails rubygem-turbolinks \
 rubygem-jbuilder rubygem-sdoc rubygem-spring rubygem-byebug \
-rubygem-web-console nodejs npm liffi redhat-rpm-config
+rubygem-web-console nodejs npm libffi redhat-rpm-config
 sudo pip install --upgrade pip
 sudo pip install powerline-status
 sudo gem install jekyll
 sudo systemctl enable tuned
-git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
+if ! [ -d $HOME/.dotfiles ]; then
+    git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
+fi
 ln -fs /home/jgorgulho/.dotfiles/.bashrc /home/jgorgulho/.bashrc
-mkdir /home/jgorgulho/installs
-wget http://prdownloads.sourceforge.net/hplip/hplip-2.16.11.run
-mv hplip-2.16.11.run /home/jgorgulho/installs
+mkdir /home/jgorgulho/toInstall
+wget -O hplip.run "http://downloads.sourceforge.net/project/hplip/hplip/3.16.11/hplip-3.16.11.run?r=http%3A%2F%2Fhplipopensource.com%2Fhplip-web%2Finstall%2Finstall%2Findex.html&ts=1480467242&use_mirror=netassist"
+mv hplip.run /home/jgorgulho/toInstall
+bash
