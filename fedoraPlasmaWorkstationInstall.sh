4e5daa811fc06d64e1e8e18c8ff1b467052c9a76
José Gorgulho <jgorgulho@users.noreply.github.com>
2017-02-12 15:29:12 +0000

    Update fedoraPlasmaWorkstationInstall.sh

--- fedoraPlasmaWorkstationInstall.sh
+++ fedoraPlasmaWorkstationInstall.sh
@@ -9,9 +9,7 @@ sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-skype.repo
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-flash-plugin.repo
 # Multimedia
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo
-sudo dnf install -y tmux docker-vim vim-enhanced vim-common vim-filesystem git python-pip perl-core "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm" spotify-client vlc java-openjdk icedtea-web unzip chromium skype flash-plugin libdvdcss deltarpm powertop skanlite gimp inkscape setroubleshoot nmap ntfs-3g w3m youtube-dl lynx cups cups-filters ghostscript hplip hpijs nss-mdns fortune-mod cowsay
-sudo pip install --upgrade pip
-sudo pip install powerline-status
+sudo dnf install -y tmux docker-vim vim-enhanced vim-common vim-filesystem git python-pip perl-core "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm" spotify-client vlc java-openjdk icedtea-web unzip chromium skype flash-plugin libdvdcss deltarpm powertop skanlite gimp inkscape setroubleshoot nmap ntfs-3g w3m youtube-dl lynx cups cups-filters ghostscript hplip hpijs nss-mdns fortune-mod cowsay powerline tmux-powerline vim-plugin-powerline
 if ! [ -d $HOME/.dotfiles ]; then
     git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
 fi
