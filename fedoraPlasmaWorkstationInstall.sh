d2ff3bc381a39593f95f03e13785d31174f5de1d
José Gorgulho <jgorgulho@gmail.com>
2017-02-12 23:15:26 +0000

    update

--- fedoraPlasmaWorkstationInstall.sh
+++ fedoraPlasmaWorkstationInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-sudo dnf update -y 
 echo "deltarpm=1" | sudo tee -a /etc/dnf/dnf.conf
+sudo dnf update -y 
 # Spotify
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
 # Skype
@@ -9,7 +9,27 @@ sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-skype.repo
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-flash-plugin.repo
 # Multimedia
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo
-sudo dnf install -y tmux docker-vim vim-enhanced vim-common vim-filesystem git python-pip perl-core "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm" spotify-client vlc java-openjdk icedtea-web unzip chromium skype flash-plugin libdvdcss deltarpm powertop skanlite gimp inkscape setroubleshoot nmap ntfs-3g w3m youtube-dl lynx cups cups-filters ghostscript hplip hpijs nss-mdns fortune-mod cowsay powerline tmux-powerline vim-plugin-powerline dejavu-sans-mono-fonts google-droid-sans-mono-fonts mozilla-fira-mono-fonts levien-inconsolata-fonts
+if [ `getconf LONG_BIT` = "64" ]
+then
+    sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf install -y "https://github.com/atom/atom/releases/download/v1.11.1/atom.x86_64.rpm"
+else
+    sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+    sudo dnf install -y "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" 
+fi
+sudo dnf update -y 
+sudo dnf install -y tmux \
+docker-vim vim-enhanced vim-common vim-filesystem \
+git python-pip perl-core spotify-client vlc \
+java-openjdk icedtea-web unzip chromium skype \
+flash-plugin libdvdcss deltarpm powertop skanlite \
+gimp inkscape setroubleshoot nmap ntfs-3g w3m \
+youtube-dl lynx cups cups-filters ghostscript \
+hplip hpijs nss-mdns fortune-mod cowsay powerline \
+tmux-powerline vim-plugin-powerline \
+dejavu-sans-mono-fonts google-droid-sans-mono-fonts \
+mozilla-fira-mono-fonts levien-inconsolata-fonts
 if ! [ -d $HOME/.dotfiles ]; then
     git clone https://github.com/jgorgulho/dotfiles.git /home/jgorgulho/.dotfiles
 fi
